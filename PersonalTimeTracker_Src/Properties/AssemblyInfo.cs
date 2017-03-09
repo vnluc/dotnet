65ba06d25e15000f2e17ffd50958c1d4679f3924
Luc Vu <vunangluc@gmail.com>
2017-03-09 11:34:34 +0700

    PersonalTimeTracker
    Personal Time Tracker that run on Windows, requires dotnet 4.0
    
    Original source of program is from (No change has been made)
    
    https://www.codeproject.com/Articles/25405/Personal-Time-Tracker

---
+++ PersonalTimeTracker_Src/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PersonalTimeTracker")]
+[assembly: AssemblyDescription("Track your time")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Vance Kessler")]
+[assembly: AssemblyProduct("PersonalTimeTracker")]
+[assembly: AssemblyCopyright("Copyright © Vance Kessler 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fff7d324-46f8-4aa5-8175-759b97325b9f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
